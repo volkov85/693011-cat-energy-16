1618fb3b342794f39fd378787a2bf45e4a71d6b4
volkov85 <xplz2007@gmail.com>
2019-04-30 17:15:48 +0600

    Исправил таблицу каталога для firefox + кнопка мобильной версии

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,10 @@ var toggler = document.querySelector(".main-nav__toggle");
 
 document.addEventListener("DOMContentLoaded", function() {
   nav.classList.remove("main-nav--show");
+  toggler.classList.add("main-nav__toggle--opened");
 });
 
 toggler.addEventListener("click", function (evt) {
   nav.classList.toggle("main-nav--show");
+  toggler.classList.toggle("main-nav__toggle--opened");
 });
