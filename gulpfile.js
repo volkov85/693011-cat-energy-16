cacea8324e04270d76531392a4ab9545a75a77e9
volkov85 <xplz2007@gmail.com>
2019-05-13 14:42:48 +0600

    Удален PixelGlass, исправления PixelPerfect

--- gulpfile.js
+++ gulpfile.js
@@ -94,7 +94,6 @@ gulp.task("copy", function () {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
-      "source/pixelglass/**",
       "source/*.ico"
     ], {
       base: "source"
